8a3d18ecbf387ce6117b685f2038bb5a22d4d9e2
Zoclark <angelcityv@gmail.com>
2025-04-11 04:14:36 +0200

    Desactivado CSRF en endpoints de API

--- padel-web/src/api/axiosConfig.js
+++ padel-web/src/api/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://padel-clases.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
