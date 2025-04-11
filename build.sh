50ae469afc4f53dac961f93176b6953021841293
Zoclark <angelcityv@gmail.com>
2025-04-11 02:00:13 +0200

    Fix build.sh y migraciones

--- build.sh
+++ build.sh
@@ -1,23 +1,5 @@
 #!/usr/bin/env bash
 
-# Instala dependencias Python
-pip install -r requirements.txt
-
-# Compila frontend React
-cd frontend
-npm install
-npm run build
-cd ..
-
-# Copia el build estático al directorio de Django
-rm -rf static
-mkdir -p static
-cp -r frontend/dist/* static/
-
-# Migrate y collectstatic
-python manage.py migrate
-python manage.py collectstatic --noinput#!/usr/bin/env bash
-
 # 1. Instalar dependencias Python
 pip install -r requirements.txt
 
@@ -29,6 +11,7 @@ cd ..
 
 # 3. Copiar build de React a Django static
 rm -rf backend/staticfiles/*
+mkdir -p backend/staticfiles
 cp -r padel-web/dist/* backend/staticfiles/
 
 # 4. Migraciones y collectstatic
