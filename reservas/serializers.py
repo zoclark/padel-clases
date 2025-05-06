d864bd0c94877f568caad7e993894dce37b1ec0e
Zoclark <angelcityv@gmail.com>
2025-05-06 07:03:44 +0200

    Emparejamientos FINAL

--- reservas/serializers.py
+++ reservas/serializers.py
@@ -172,6 +172,7 @@ class UsuarioPerfilSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
         fields = [
+            "id",
             "username",
             "email",
             "first_name",
